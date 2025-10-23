9bfd31fcfb4ba05b8007484d5d16a1749566afaf
Marcelo <darmismarcelo@yahoo.com.ar>
2025-10-23 16:07:57 +0100

    fullstack - make a bit better location types

--- front/src/components/LocationSelector.tsx
+++ front/src/components/LocationSelector.tsx
@@ -1,4 +1,9 @@
-const locations = {
+type Coordinates = [number, number];
+type LocationMap = {
+  [city in "Edinburgh" | "London" | "NewYork"]: Coordinates;
+};
+
+const locations: LocationMap = {
   Edinburgh: [55.9533, -3.1883],
   London: [51.5074, -0.1278],
   NewYork: [40.7128, -74.006],
@@ -7,18 +12,17 @@ const locations = {
 export const LocationSelector = ({
   onSelect,
 }: {
-  onSelect: (loc: [number, number]) => void;
+  onSelect: (coordinates: Coordinates) => void;
 }) => {
   return (
     <select
-      onChange={(e) =>
-        onSelect(
-          locations[e.target.value as keyof typeof locations] as [
-            number,
-            number
-          ]
-        )
-      }
+      onChange={(e) => {
+        // The select value will be one of our city names
+        const city = e.target.value as keyof LocationMap;
+        // Get the coordinates for the selected city
+        const coordinates: Coordinates = locations[city];
+        onSelect(coordinates);
+      }}
     >
       {Object.keys(locations).map((loc) => (
         <option key={loc} value={loc}>
